9e501383e81d2424c674b59029d9dd5100a5d9d9
maggu <maggu@puffincreek.com>
2018-11-15 21:39:35 -0500

    INIT-001 - Add in docker profile

---
+++ build.sh
@@ -0,0 +1,43 @@
+# Thumb Rules:
+# Build order matters.
+
+#go for build
+declare -a modules=(
+    "versioning" 
+    "reverse-engineering"
+    "core-domain"
+    "core-rest"
+    "config"
+    "gateway"
+    "enterprise"
+    "settings"
+    "identity"
+)
+
+declare -A results
+
+pushd ..
+
+for module in "${modules[@]}"
+do
+        pushd ${module}
+        mvn clean install
+	if [ "$?" -eq 0 ]; 
+		then results[${module}]="SUCCESS"; 
+		else results[${module}]="FAILURE"; break; 
+	fi
+        popd
+done
+
+popd
+
+printf '=%.0s' {1..70}
+printf "\n%-50s | %-20s\n" "MODULE" "RESULT"
+printf '=%.0s' {1..70}
+printf '\n'
+for module in "${modules[@]}"
+do
+	printf "%-50s | %-20s\n" "$module" "${results[$module]}"
+done
+printf '=%.0s' {1..70}
+printf '\n'
