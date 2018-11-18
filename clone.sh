6b650fcc1b3420e68b5dc90de2460e0fcce6f429
jack <jack@puffincreek.com>
2018-11-18 12:32:35 -0500

    init-001 add clone script

---
+++ clone.sh
@@ -0,0 +1,41 @@
+# Thumb Rules:
+# Build order matters.
+
+#go for build
+declare -a repos=(
+    "git@github.com:puffincreek/settings.git"
+    "git@github.com:puffincreek/enterprise.git"
+    "git@github.com:puffincreek/config.git"
+    "git@github.com:puffincreek/vault.dev.git"
+    "git@github.com:puffincreek/identity.git"
+    "git@github.com:puffincreek/versioning.git"
+    "git@github.com:puffincreek/reverse-engineering.git"
+    "git@github.com:puffincreek/gateway.git"
+    "git@github.com:puffincreek/core-rest.git"
+    "git@github.com:puffincreek/core-domain.git"
+)
+
+declare -A results
+pushd ..
+
+for repo in "${repos[@]}"
+do	
+    git clone ${repo}        
+	if [ "$?" -eq 0 ];
+		then results[${repo}]="SUCCESS"; 
+		else results[${repo}]="FAILURE"; break; 
+	fi
+done
+
+popd
+
+printf '=%.0s' {1..70}
+printf "\n%-50s | %-20s\n" "MODULE" "RESULT"
+printf '=%.0s' {1..70}
+printf '\n'
+for repo in "${repos[@]}"
+do
+	printf "%-50s | %-20s\n" "$repo" "${results[$repo]}"
+done
+printf '=%.0s' {1..70}
+printf '\n'
